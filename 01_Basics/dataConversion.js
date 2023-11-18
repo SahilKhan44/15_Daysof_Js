a4cb0351314b91895b8989dbc3a58e41490aaae8
Sahil khan <116485673+SahilKhan44@users.noreply.github.com>
2023-11-18 06:16:41 +0000

    datatype

---
+++ 01_Basics/dataConversion.js
@@ -0,0 +1,15 @@
+let score = "33ab";
+
+
+console.log(typeof(score));
+
+console.log(typeof score);
+
+let Value = Number(score);
+ 
+console.log(typeof Value);
+
+console.log(Value);
+//33 =="33"
+//N"33abc" = NaN
+//true = 1 else 0
---
+++ 01_Basics/dataSummary.js
@@ -0,0 +1,22 @@
+/*
+
+primitive datatype
+
+number, string , boolean ,null, undefined,symbol,bigInt
+
+why we called primitive because tey change in copy value not in actual value.
+
+non promintive or refrenece type 
+
+Array, objects,functions
+const hero = ["sahil","Khan"];
+
+let hero = {
+    name : "Sahil",
+    age: "kah",
+}
+const hero = function(){
+    console.log("Hello");
+}
+
+*/
